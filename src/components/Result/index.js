a479cc09e4fd7f772d825518afd071491c773e69
Rune Molin <rmo@nineconsult.dk>
2018-06-16 10:09:06 +0200

    Adapted to changes in virkr search resulat

--- src/components/Result/index.js
+++ src/components/Result/index.js
@@ -43,19 +43,19 @@ export default class Result extends React.Component {
       return null
     }
 
-    const {virksomheder, deltagere} = resultat
+    const {virksomhedSoegeresultat, deltagerSoegeresultat} = resultat
 
     return (
       <div>
         <br/>
         <div className='row'>
           <div className='col-md-12'>
-            <Personer data={deltagere}/>
+            <Personer data={deltagerSoegeresultat.deltagere}/>
           </div>
         </div>
         <div className='row'>
           <div className='col-md-12'>
-            <Virksomheder data={virksomheder} vaelgCvrNr={this.vaelgCvrNr}/>
+            <Virksomheder data={virksomhedSoegeresultat.virksomheder} vaelgCvrNr={this.vaelgCvrNr}/>
           </div>
         </div>
       </div>
